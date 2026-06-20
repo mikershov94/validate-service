c1266f909eebbe2e8249b2edd8aa7497fe6616e3
Michael E. Ershov <i@emershov.ru>
2026-06-20 20:25:15 +0800

    created jobs processor

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { JobsController } from './jobs.controller';
 import { JobsService } from './jobs.service';
 import { JobsRepository } from './repository/jobs.repository';
+import { JobsProcessorService } from './processors/jobs-processor.service';
 
 @Module({
     controllers: [JobsController],
-    providers: [JobsService, JobsRepository],
+    providers: [JobsService, JobsRepository, JobsProcessorService],
 })
 export class JobsModule {}
