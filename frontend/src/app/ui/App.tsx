608037929f68a37d47b06fbf6c1c7bf0d7a21b2e
Michael E. Ershov <i@emershov.ru>
2026-06-21 21:45:55 +0800

    installed storybook

--- frontend/src/app/ui/App.tsx
+++ frontend/src/app/ui/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@mantine/core';
+
 function App() {
-    return <></>;
+    return (
+        <>
+            <div>Hello</div>
+            <Button>Hello</Button>
+        </>
+    );
 }
 
 export { App };
